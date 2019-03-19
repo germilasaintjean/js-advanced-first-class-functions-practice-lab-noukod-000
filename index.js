6d4e8682cac1965633498b246a3622c1d392c8c8
Saint-Jean Germila <germila@noukod.com>
2019-03-19 10:33:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+const logDriverNames = function(el, i, drivers){
+  console.log(el)
+  console.log(i)
+  console.log(drivers)
+  console.log(-------)
+}
+drivers.forEach(logDriverNames;)
